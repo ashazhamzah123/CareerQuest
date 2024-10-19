3af3c2b5205f8a9c22313aa27fceda6af5a62559
ashazhamzah123 <ashazhamzah123@gmail.com>
2024-10-19 19:00:13 +0530

    2.1

--- server/careerquest/jobs/admin.py
+++ server/careerquest/jobs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, JobListing, Application, Branch
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(JobListing)
+admin.site.register(Application)
+admin.site.register(Branch)
