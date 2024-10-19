df3b9405a969e9ef2a2c57ea89c54f73c96f2d30
ashazhamzah123 <ashazhamzah123@gmail.com>
2024-10-19 19:22:29 +0530

    added react to the project

--- server/careerquest/careerquest/settings.py
+++ server/careerquest/careerquest/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # Or any other domain if you host it differently
+    "http://localhost:5173",  # Or any other domain if you host it differently
 ]
 
 ROOT_URLCONF = 'careerquest.urls'
